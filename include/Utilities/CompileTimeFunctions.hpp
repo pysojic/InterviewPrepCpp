4291454cf5dead9fe5b1b4f5c6e6467a35b40cb5
pysojic <pysojic@uchicago.edu>
2025-07-04 13:04:42 +0200

    Added some metafunctions

--- include/Utilities/CompileTimeFunctions.hpp
+++ include/Utilities/CompileTimeFunctions.hpp
@@ -1,31 +1,32 @@
 #pragma once
 
 #include <tuple>
+#include <type_traits>
 
 // Function that returns the size of a tuple at compile time
 
 template <typename TList>
-struct Length;
+struct LengthTuple;
 
 template<>
-struct Length<std::tuple<>>
+struct LengthTuple<std::tuple<>>
 {
     static constexpr size_t value = 0;
 };
 
 template <typename T, typename... Us>
-struct Length<std::tuple<T, Us...> >
+struct LengthTuple<std::tuple<T, Us...> >
 {
-    static constexpr size_t value = 1 + Length<std::tuple<Us...>>::value;
+    static constexpr size_t value = 1 + LengthTuple<std::tuple<Us...>>::value;
 };
 
 // Function that appends the types of two tuple at compile time
 
 template <typename First, typename Second>
-struct Append;
+struct AppendTuple;
 
 template <typename... Ts, typename... Us>
-struct Append<std::tuple<Ts...>, std::tuple<Us...>>
+struct AppendTuple<std::tuple<Ts...>, std::tuple<Us...>>
 {
     using type = std::tuple<Ts..., Us...>;
 };
@@ -49,3 +50,127 @@ struct Dimension<T[N]>
 {
     static constexpr size_t value = 1 + Dimension<T>::value;
 };
+
+template <int... Args>
+struct CompileTimeVector {};
+
+//---------- Print ----------
+
+template<int Head>
+constexpr void print(CompileTimeVector<Head>)
+{
+    std::cout << Head << std::endl;
+}
+
+template<int Head, int... Args>
+constexpr void print(CompileTimeVector<Head, Args...> args)
+{
+    std::cout << Head << ' ';
+    print(CompileTimeVector<Args...>{});
+}
+
+//---------- Prepend ----------
+
+template<int Pre, typename Seq>
+struct Prepend;
+
+template <int Pre, template<int...> class Seq, int... Args>
+struct Prepend<Pre, Seq<Args...>>
+{
+    using type = Seq<Pre, Args...>;
+};
+
+template<int Pre, typename... Args>
+using Prepend_t = typename Prepend<Pre, Args...>::type;
+
+//---------- Append ----------
+
+template<int Post, typename Seq>
+struct Append;
+
+template <int Post, template<int...> class Seq, int... Args>
+struct Append<Post, Seq<Args...>>
+{
+    using type = Seq<Args..., Post>;
+};
+
+//---------- PopBack ----------
+
+template<typename Seq>
+struct PopBack;
+
+template<template<int...> class Seq, int T>
+struct PopBack<Seq<T>>
+{
+    using type = Seq<>;
+};
+
+template<template<int...> class Seq, int First, int... Rest>
+struct PopBack<Seq<First, Rest...>>
+{
+    using TailPopped = typename PopBack<Seq<Rest...>>::type;
+
+    using type = typename Prepend<First, TailPopped>::type;
+};
+
+//---------- RemoveFirst ----------
+
+template <int First, typename Seq>
+struct RemoveFirst;
+
+template <template<int...> class Seq, int First, int... Tail>
+struct RemoveFirst<First, Seq<First, Tail...>>
+{
+    using type = Seq<Tail...>;
+};
+
+template <template<int...> class Seq, int H, int First, int... Tail>
+struct RemoveFirst<First, Seq<H, Tail...>>
+{
+    using Rest = typename RemoveFirst<First, Seq<Tail...>>::type;
+    using type = Prepend_t< H, Rest >;
+};
+
+//---------- RemoveAll ----------
+
+template <int T, typename Seq>
+struct RemoveAll;
+
+template<int T, template<int...> class Seq>
+struct RemoveAll<T, Seq<>>
+{
+    using type = Seq<>;
+};
+
+template <int T, template<int...> class Seq, int... Tail>
+struct RemoveAll<T, Seq<T, Tail...>>
+{
+    using type = typename RemoveAll<T, Seq<Tail...>>::type;
+};
+
+template <int T, template<int...> class Seq, int Head, int... Tail>
+struct RemoveAll<T, Seq<Head, Tail...>>
+{
+    using Rest = typename RemoveAll<T, Seq<Tail...>>::type;
+    using type = typename Prepend<Head, Rest>::type;
+};
+
+//---------- Length ----------
+
+template<typename Seq>
+struct Length;
+
+template<template<int...> class Seq>
+struct Length<Seq<>>
+{
+    constexpr static size_t value = 0;
+};
+
+template<template<int...> class Seq, int Head, int... Tail>
+struct Length<Seq<Head, Tail...>>
+{
+    constexpr static size_t value = 1 + Length<Seq<Tail...>>::value;
+};
+
+template<typename Seq>
+static constexpr std::size_t Length_t = Length<Seq>::value;
--- src/test.cpp
+++ src/test.cpp
@@ -17,72 +17,18 @@
 #include "Utilities/Any.hpp"
 #include "Utilities/CompileTimeFunctions.hpp"
 
-struct testClass
-{
-    testClass() { std::cout << "default" << std::endl; };
-    testClass(int a1, int a2) : a{a1}, b{a2} { std::cout << "construct with params" << std::endl; }
-    testClass(const testClass& other) : a{other.a}, b{other.b} { std::cout << "copy" << std::endl;}
-    testClass(testClass&& other) : a{other.a}, b{other.b} { std::cout << "move" << std::endl;}
-    testClass& operator=(const testClass& other)
-    {
-        if (this != &other)
-        {
-            a = other.a;
-            b = other.b;
-        }
-
-        std::cout << "copy ass" << std::endl;
-
-        return *this;
-    }
-    testClass& operator=(testClass&& other)
-    {
-        if (this != &other)
-        {
-            a = std::move(other.a);
-            b = std::move(other.b);
-        }
-
-         std::cout << "move ass" << std::endl;
-
-        return *this;
-    }
-    ~testClass() { std::cout << "destroy test (" << a << ',' << b << ')' << std::endl; }
-
-    friend std::ostream& operator << (std::ostream& os, const testClass& test)
-    {
-        os << '[' << test.a << ',' << test.b << ']';
-        return os;
-    }
-
-    void print() const { std::cout << '[' << a << ',' << b << ']' << std::endl; }
-
-    int a;
-    int b;
-};
-
-SpinLock sp;
-
-void test()
-{
-    sp.lock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    sp.unlock();
-}
-
-template <typename... Args>
-decltype(auto) add(Args&&... args)
-{
-    return (std::forward<Args>(args) + ...);
-}
-
 int main()
 {
-    using CharTypes = std::tuple<char16_t, char32_t, unsigned char>;
-    using NumTypes = std::tuple<int, unsigned int, long, long long>;
-    std::cout << Length<CharTypes>::value << std::endl;
-    using CombinedTypes = Append<CharTypes, NumTypes>::type;
-    std::cout << Length<CombinedTypes>::value << std::endl;
+    static_assert(std::is_same_v<CompileTimeVector<1, 2>, CompileTimeVector<1,2>>);
+    print(CompileTimeVector<1,2,3,4,5>{});
+    static_assert(std::is_same_v<Prepend_t<1, CompileTimeVector<2,3>>, CompileTimeVector<1,2,3>>);
+    static_assert(std::is_same_v< Append<4, CompileTimeVector<1,2,3>>::type , CompileTimeVector<1,2,3,4> >);
+    static_assert(std::is_same_v< PopBack<CompileTimeVector<1,2,3,4>>::type , CompileTimeVector<1,2,3> >);
+    static_assert(std::is_same_v<RemoveFirst<1, CompileTimeVector<1,1,2>>::type, CompileTimeVector<1,2>>);
+    static_assert(std::is_same_v<RemoveFirst<2, CompileTimeVector<1,1,2>>::type, CompileTimeVector<1,1>>);
+    static_assert(std::is_same_v<RemoveAll<9, CompileTimeVector<1,9,2,9,3,9>>::type, CompileTimeVector<1,2,3>>);
+    static_assert(Length_t<CompileTimeVector<>> == 0);
+    static_assert(Length_t<CompileTimeVector<1,2,3>> == 3);
 
     std::cout << Dimension<int>::value << std::endl;
 }
